bbbc56ce29950ec2742d4736a3966f76a0e9566e
Stefan Schärmeli <info@schaermu.ch>
2021-05-28 11:14:21 +0200

    style: fixes linter issues in config package

--- config/config.go
+++ config/config.go
@@ -31,10 +31,12 @@ scorers:
     max_penalty: 2.0
 `)
 
+// GetDefaultConfig returns the static default config.
 func GetDefaultConfig() []byte {
 	return defaultConfig
 }
 
+// SlackConfig configures the Slack client's connection to a slack team.
 type SlackConfig struct {
 	BotToken   string `mapstructure:"bot_token"`
 	AppToken   string `mapstructure:"app_token"`
@@ -42,18 +44,13 @@ type SlackConfig struct {
 	Debug      bool   `mapstructure:"debug"`
 }
 
+// GithubConfig configures authenticated access to Github's API.
 type GithubConfig struct {
 	Username            string `mapstructure:"username"`
 	PersonalAccessToken string `mapstructure:"access_token"`
 }
 
-type Config struct {
-	Slack     SlackConfig    `mapstructure:"slack"`
-	Github    GithubConfig   `mapstructure:"github"`
-	Scorers   []ScorerConfig `mapstructure:"scorers"`
-	DebugMode bool           `mapstructure:"debug"`
-}
-
+// ScorerConfig configures a scorer.
 type ScorerConfig struct {
 	Name       string            `mapstructure:"name"`
 	MaxPenalty float64           `mapstructure:"max_penalty"`
@@ -61,6 +58,15 @@ type ScorerConfig struct {
 	Settings   map[string]string `mapstructure:"settings"`
 }
 
+// Config is the main configuration object.
+type Config struct {
+	Slack     SlackConfig    `mapstructure:"slack"`
+	Github    GithubConfig   `mapstructure:"github"`
+	Scorers   []ScorerConfig `mapstructure:"scorers"`
+	DebugMode bool           `mapstructure:"debug"`
+}
+
+// Get returns a string value for a setting key.
 func (s ScorerConfig) Get(key string) string {
 	if val, found := s.Settings[key]; found {
 		return val
@@ -68,6 +74,7 @@ func (s ScorerConfig) Get(key string) string {
 	return ""
 }
 
+// GetFloat64 returns a float64 value for a setting key.
 func (s ScorerConfig) GetFloat64(key string) float64 {
 	val := s.Get(key)
 	if val != "" {
@@ -76,6 +83,7 @@ func (s ScorerConfig) GetFloat64(key string) float64 {
 	return 0
 }
 
+// GetInt returns a int value for a setting key.
 func (s ScorerConfig) GetInt(key string) int {
 	val := s.Get(key)
 	if val != "" {
@@ -84,6 +92,7 @@ func (s ScorerConfig) GetInt(key string) int {
 	return 0
 }
 
+// GetSlice returns a string slice value for a setting key.
 func (s ScorerConfig) GetSlice(key string) []string {
 	val := s.Get(key)
 	if val != "" {
@@ -92,6 +101,7 @@ func (s ScorerConfig) GetSlice(key string) []string {
 	return []string{}
 }
 
+// New deserializeses an io.Reader input into a Config object.
 func New(rdr io.Reader) (config Config, err error) {
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
